924d1f89a4f8a5ca3c705a11a0c93a6d5660c913
joaovnpt <joaovitorguiotti8@gmail.com>
2025-03-19 21:57:44 -0300

    Feat: Create ProfilePicture component

---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <div class="profile-container">
+    <img src="../assets/pfp sem fndo.png" alt="" />
+  </div>
+  <div class="pfp-effect">
+    <img src="../assets/pfp fx.png" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.profile-container {
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+  border: 1px solid #000;
+  border-radius: 100%;
+}
+.profile-container img {
+  display: block;
+  width: 350px;
+  height: 350px;
+  border-radius: 100%;
+}
+.pfp-effect {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: fit-content;
+  height: fit-content;
+  z-index: -1;
+}
+.pfp-effect img {
+  width: 500px;
+}
+</style>
