dec9a117874e95d951e04b652b1be019af600955
joaovnpt <joaovitorguiotti8@gmail.com>
2025-03-19 22:26:26 -0300

    Feat: Create SkillsComponent

---
+++ src/components/SkillsComponent.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <div class="container"></div>
+</template>
+
+<style scoped>
+.container {
+    height: 100vh;
+    background-color: var(--main-color);
+}
+</style>
