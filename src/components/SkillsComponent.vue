5252f3cde07dae1e95d6126e55585b9994d893ca
joaovnpt <joaovitorguiotti8@gmail.com>
2025-03-20 14:43:05 -0300

    Feat: Create SkillsComponent and add icons to the component

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -1,12 +1,75 @@
 <script setup></script>
 
 <template>
-    <div class="container"></div>
+  <div class="container">
+    <div class="main-skills">
+      <div class="description">
+        <h1>Main skills</h1>
+        <p>
+          I usually create modern and responsive interfaces using HTML, CSS, JavaScript, Vue.js,
+          Tailwind CSS, Figma and sometimes Django for my design-to-code workflows.
+        </p>
+      </div>
+      <div class="icons">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg"
+        />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+      </div>
+    </div>
+    <div class="other-knowledge"></div>
+  </div>
 </template>
 
 <style scoped>
 .container {
-    height: 100vh;
-    background-color: var(--main-color);
+  padding: 150px;
+  height: 100vh;
+  background-color: var(--main-color);
+  overflow: hidden;
+}
+.description {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 400px;
+  line-height: 1.4em;
+}
+h1 {
+  color: var(--main-text-color);
+}
+p {
+  color: #ffff;
+  font-weight: 300;
+}
+.icons {
+  display: flex;
+  margin-top: 2rem;
+  gap: 0.8rem;
+}
+.icons img {
+  width: 40px;
+  transition: 300ms all;
+}
+.icons img:hover {
+    transform: translateY(-20px);
 }
 </style>
