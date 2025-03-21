32ffc363606cb802290b4feb5691893b29daefad
joaovnpt <joaovitorguiotti8@gmail.com>
2025-03-21 00:19:13 -0300

    Feat: Create 'in development' pages

---
+++ src/components/ProjectsComponent.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div class="projects-container">
+    <div class="temporary">
+        <h1>Projects area still in development...</h1>
+        <p>But some of my projects are public on Github, check on the link.</p>
+        <a target="blank" href="https://github.com/joaovnpt">joaovnpt on Github</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.projects-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+a {
+    color: #000;
+}
+</style>
---
+++ src/views/InDevelopment.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <h1>This area still in development, sorry.</h1>
+    <img src="/development-area.svg" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5rem;
+}
+img {
+  width: 500px;
+}
+</style>
