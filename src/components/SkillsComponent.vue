de6e5afea4cdc418d2c67285c6014051aadb01c6
joaovnpt <joaovitorguiotti8@gmail.com>
2025-03-21 22:13:57 -0300

    Feat: Change After Effects icon, background color and h1 style

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -84,7 +84,7 @@
         </div>
         <div class="icon longer-icon">
           <img
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/aftereffects/aftereffects-plain.svg"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/aftereffects/aftereffects-original.svg"
           />
           <div class="text">After Effects</div>
         </div>
@@ -109,7 +109,12 @@
   justify-content: space-between;
   padding: 150px;
   height: 80vh;
-  background-color: var(--main-color);
+  /* background-color: var(--main-color); */
+  background: radial-gradient(
+    var(--main-color-100) 10%,
+    var(--main-color-200) 80%,
+    var(--main-color) 100%
+  );
 }
 .main-skills {
   display: flex;
@@ -129,13 +134,39 @@
 }
 h1 {
   color: #e1e1e1;
-  border: 2px solid #1d1d1d;
-  border-radius: 15px 15px 0;
+  border: 1px solid #3d3d3d;
+  border-radius: 10px;
   width: fit-content;
+  background-color: #4b4b4b;
   padding: 1rem;
+  text-shadow: 1px 1px 2px #0000001b;
+  transition: all 250ms;
+  box-shadow:
+    inset 0 1px 0 0 #5a5a5a,
+    0 1px 0 0 #505050,
+    0 2px 0 0 #4d4d4d,
+    0 4px 0 0 #4a4a4a,
+    0 5px 0 0 #3f3f3f,
+    0 6px 0 0 #3d3d3d,
+    0 7px 0 0 #3b3b3b,
+    0 7px 8px 0 #3a3a3a;
+  cursor: pointer;
+}
+h1:hover {
+  box-shadow:
+    inset 0 1px 0 0 #5a5a5a,
+    0 1px 0 0 #505050,
+    0 1px 0 0 #4d4d4d,
+    0 2px 0 0 #4a4a4a,
+    0 2px 0 0 #3f3f3f,
+    0 3px 0 0 #3d3d3d,
+    0 4px 0 0 #3b3b3b,
+    0 4px 6px 0 #3a3a3a;
+  transform: translateY(4px);
 }
 p {
   color: #ffff;
+  text-shadow: 1px 1px 1px #00000043;
   font-weight: 300;
 }
 .icons {
